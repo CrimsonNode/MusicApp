68cc2ac201708b94c53cec90e63179b4807f85a2
FatihEfeAydogan <efefatihaydogan@gmail.com>
2024-04-25 21:20:17 +0300

    struct
    property baslangıc

---
+++ Vy_Proje.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct{
+    int singer_id;
+    char singerName;
+    char singerSurname; 
+    Song song;
+    int song;
+}Singer;
+
+typedef struct{
+    int song_id;
+    int listenCount;
+    Singer singer;
+    SongType songtype;
+    int song_type;
+    int singer_name;
+}Song;
+
+
+typedef struct{
+    int songType_id;
+    char songType;
+}SongType;
+
+
+
+
+
+
+
