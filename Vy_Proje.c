a6fe761536b35417c454e6a232109306fe757135
Emir Uçar <emirucar16@hotmail.com>
2024-04-26 09:59:49 +0300

    Yapi Degisikligi
    Yapida degisiklik yapildi

--- Vy_Proje.c
+++ Vy_Proje.c
@@ -1,29 +1,58 @@
+/* 
+ESKİ YAPİ
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 typedef struct{
-    int singer_id;
-    char* singerName;
-    char* singerSurname; 
-    Song song;
-    char* song;
+int singer_id;
+char* singerName;
+char* singerSurname;
+Song song;
+char* song;
 }Singer;
 
 typedef struct{
-    int song_id;
-    int listenCount;
-    Singer singer;
-    SongType songtype;
-    char* song_type;
-    char* singer_name;
+int song_id;
+int listenCount;
+Singer singer;
+SongType songtype;
+char* song_type;
+char* singer_name;
 }Song;
 
-
 typedef struct{
+int songType_id;
+char* songType;
+}SongType;
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct SongType {
     int songType_id;
     char* songType;
-}SongType;
+} SongType;
+
+typedef struct Singer {
+    int singer_id;
+    char* singerName;
+    char* singerSurname;
+    struct Song* song; 
+} Singer;
+
+typedef struct Song {
+    int song_id;
+    int listenCount;
+    struct Singer singer; 
+    struct SongType songType; 
+} Song;
+
+
+
 
 
 
